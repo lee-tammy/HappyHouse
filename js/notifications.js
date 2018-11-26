6ae4af673b86de5fadcf3706024df1e137ec5f5e
Tammy Lee <tal066@ucsd.edu>
2018-11-25 23:56:15 -0800

    notifications work - can delete them too

--- js/notifications.js
+++ js/notifications.js
@@ -62,6 +62,8 @@ function completedTaskNote() {
 }*/
 
 function refreshPage(){
+  $( ".notification-group" ).remove();
+
   //compile the template
   var source = $("#entry-notifications").html();
   var template = Handlebars.compile(source);
@@ -72,7 +74,6 @@ function refreshPage(){
 
   var user = (JSON.parse(localStorage.getItem("current-user")))['user-name'];
   var newComplexData = JSON.parse(localStorage.getItem(user + "-notifications"));
-console.log(newComplexData);
   if (newComplexData != null) {
     for (var i = (newComplexData.length - 1); i >= 0; i--) {
       var curData = newComplexData[i];
@@ -80,4 +81,25 @@ console.log(newComplexData);
       parentDiv.append(curHtml);
     }
   } 
+}
+
+function deleteNotification(button){
+  var parent = button.parentNode;
+  var grandFather = parent.parentNode;
+  grandFather.removeChild(parent);
+
+  var children = parent.childNodes;
+  var taskName = children[3].innerHTML;
+  var type = children[5].innerHTML;
+
+  var currentUser = (JSON.parse(localStorage.getItem("current-user")))["user-name"];
+  var notifications = JSON.parse(localStorage.getItem(currentUser + "-notifications"));
+  var filtered = notifications.filter(function(value){
+    if(value["taskName"] != taskName || value["type"] != type){
+      return value;
+    }
+  })
+
+  localStorage.setItem(currentUser + "-notifications", JSON.stringify(filtered));
+  refreshPage();
 }
